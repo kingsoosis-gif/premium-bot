0fa48da0eb0b139d25108aec27d7341afab4e5d5
kingsoosis-gif <kingsoosis@gmail.com>
2025-11-14 18:41:04 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-import os
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
@@ -9,9 +8,11 @@ from config import BOT_TOKEN
 from database import init_db
 from keyboards import main_menu
 
-# فقط این هندلرها الان وجود دارن
+# همه هندلرها
 from handlers.start import router as start_router
 from handlers.categories import router as categories_router
+from handlers.services import router as services_router
+from handlers.order import router as order_router
 from handlers.wallet import router as wallet_router
 from handlers.profile import router as profile_router
 from handlers.admin import router as admin_router
@@ -23,18 +24,17 @@ dp = Dispatcher()
 
 async def on_startup():
     await init_db()
-    print("بات با موفقیت روشن شد! 🚀")
+    print("بات با موفقیت روشن شد! 🚀 همه سرویس‌ها و قیمت‌ها آماده‌ست 💰")
 
 async def main():
-    # ثبت فقط هندلرهای موجود
+    # ثبت همه روترها
     dp.include_router(start_router)
     dp.include_router(categories_router)
+    dp.include_router(services_router)
+    dp.include_router(order_router)
     dp.include_router(wallet_router)
     dp.include_router(profile_router)
     dp.include_router(admin_router)
-
-    from handlers.services import router as services_router
-dp.include_router(services_router)
 
     dp.startup.register(on_startup)
     await dp.start_polling(bot)
