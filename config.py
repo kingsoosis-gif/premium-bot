ebb459920ca8b61351e1c8b384fd2492eed19655
kingsoosis-gif <kingsoosis@gmail.com>
2025-11-14 16:39:00 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+BOT_TOKEN = "8364048690:AAH9_IlFR25KS5NhNbnjX_vzNd_PJ4zHCks"
+
+# اولین باری که /admin بزنی خودش آیدی تو رو ذخیره می‌کنه
+ADMIN_IDS = []
+
+# تنظیمات اولیه (بعداً از داخل بات با /admin تغییر می‌دی)
+CARD_NUMBER = "6037-9973-XXXX-XXXX"
+CARD_OWNER = "نام صاحب کارت"
+PROFIT_PERCENT = 28  # درصد سود تو
+DOLLAR_RATE = 62000  # موقتاً، بعداً خودکار آپدیت می‌شه
+WELCOME_TEXT = "به فروشگاه پرمیوم خوش اومدی ❤️"
